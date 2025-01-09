aa685e2b4f0ebdf4efd3596852ab85c019660879
Lylian-violeau <Lylian.violeau@univ-tlse3.fr>
2025-01-09 08:27:35 +0100

    Correction gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,15 @@
 package personnages;
 
 public class Gaulois {
-
+	String nom;
+	Integer force;
+	
+	public Gaulois(String nom, Integer force){
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
 }
